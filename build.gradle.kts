5e79a2e05a770821b707701207fae0713a3a5700
Denis Timakov <timakden88@gmail.com>
2021-10-13 10:18:54 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.39.0"
-    kotlin("jvm") version "1.5.30"
+    kotlin("jvm") version "1.5.31"
 }
 
 group = "ru.timakden"
@@ -14,7 +14,7 @@ repositories {
     mavenCentral()
 }
 
-val kotestVersion = "4.6.2"
+val kotestVersion = "4.6.3"
 val tinylogVersion = "2.3.2"
 
 dependencies {
