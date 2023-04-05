4a60ee581f3a88a86e8acadb53492ae81590189a
Denis Timakov <timakden88@gmail.com>
2023-04-05 12:37:09 +0300

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.46.0"
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "ru.timakden"
