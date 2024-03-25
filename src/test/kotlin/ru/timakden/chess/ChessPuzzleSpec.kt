aa63cda712d7cffa8e12844d4c08eba8d7fdca3e
Denis Timakov <timakden88@gmail.com>
2024-03-25 10:57:15 +0300

    Refactor ChessPuzzle test cases to use tuples
    The test cases in `ChessPuzzleSpec` are updated to use the `tuple` function instead of Kotlin's Pair class. This enhances the readability of the test cases and simplifies the inputs and expected outcomes representation in the `withData` function.

--- src/test/kotlin/ru/timakden/chess/ChessPuzzleSpec.kt
+++ src/test/kotlin/ru/timakden/chess/ChessPuzzleSpec.kt
@@ -2,19 +2,20 @@ package ru.timakden.chess
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.tuple
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class ChessPuzzleSpec : FunSpec({
     context("ChessPuzzle") {
         withData(
-            nameFn = { "input = ${it.first}, expected = ${it.second}" },
+            nameFn = { "input = ${it.a}, expected = ${it.b}" },
             ts = listOf(
-                ChessPuzzle(8, 8, 0, 8, 0, 0, 0) to 92,
-                ChessPuzzle(11, 11, 0, 11, 0, 0, 0) to 2680,
-                ChessPuzzle(3, 3, 2, 0, 1, 0, 0) to 4,
-                ChessPuzzle(4, 4, 0, 0, 2, 0, 4) to 8,
-                ChessPuzzle(6, 9, 2, 1, 1, 1, 1) to 20136752
+                tuple(ChessPuzzle(8, 8, 0, 8, 0, 0, 0), 92),
+                tuple(ChessPuzzle(11, 11, 0, 11, 0, 0, 0), 2680),
+                tuple(ChessPuzzle(3, 3, 2, 0, 1, 0, 0), 4),
+                tuple(ChessPuzzle(4, 4, 0, 0, 2, 0, 4), 8),
+                tuple(ChessPuzzle(6, 9, 2, 1, 1, 1, 1), 20136752)
             )
         ) { (puzzle, expected) ->
             shouldNotThrowAny { puzzle.solve() }
