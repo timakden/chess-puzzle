70ab226c6227c20f023ad166f36a87628b011a0c
Denis Timakov <timakden88@gmail.com>
2024-08-23 10:25:39 +0300

    Update Gradle to 8.10 and improve workflow setup
    Upgraded the Gradle distribution URL and wrapper version to 8.10. Modified GitHub Actions workflow to use a more streamlined setup with updated Gradle actions and simplified the test run command. Also updated Kotlin plugin to version 2.0.20.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 plugins {
     idea
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.0.10"
+    kotlin("jvm") version "2.0.20"
 }
 
 group = "ru.timakden"
@@ -51,7 +51,7 @@ tasks {
         useJUnitPlatform()
     }
     wrapper {
-        gradleVersion = "8.9"
+        gradleVersion = "8.10"
     }
 }
 
