9ff3cd2c0f57f7c4e47ebf5f062e6d58c6f979c7
Denis Timakov <timakden@icloud.com>
2025-12-16 09:44:21 +0300

    Add plugin management and configure Foojay toolchains resolver in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "chess-puzzle"
